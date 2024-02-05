75e53ebed8a8efb8b458d1f23ecfb27aab5089ad
Sergey Kamalyan <qamalyan2021@inbox.ru>
2024-02-05 18:57:25 +0400

    Initialize application

---
+++ src/libs/ui/atoms/Button/index.tsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+interface ButtonProps {
+    // Define your props here
+}
+
+export const Button = memo((props: ButtonProps) => {
+    return (
+        <button>asd</button>
+    );
+});
